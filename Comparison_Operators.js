477b68f18b5a65e2adabd49f357896dc215c5fdb
divya-chandru <divya.anbazhakan@gmail.com>
2026-04-01 16:06:48 +0530

    Add files via upload

---
+++ Comparison_Operators.js
@@ -0,0 +1,10 @@
+ //Comparison Operators
+ let a=10
+console.log(a==10);
+console.log(a!=10);
+console.log(a<10);
+console.log(a>8);
+console.log(a<=12);
+console.log(a>=10);
+console.log(a===100);
+console.log(a!==10)
